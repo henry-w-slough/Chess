87670f3dc01c596faf96727c3d75b18fdaae19fc
Henry S <henry.w.slough@gmail.com>
2025-04-11 10:25:39 -0400

    commit 6

--- Chess/src/App.java
+++ Chess/src/App.java
@@ -29,41 +29,48 @@ public class App {
 		GameHandler gameHandler = new GameHandler();
 
 
-		HashMap<String, Piece> piecesMap = new HashMap<>();
-
-		piecesMap.put("K", new Piece(gamePanel.loadImage("/sprites/w_king.png"), "king", 0, 0, 0));
-		piecesMap.put("Q", new Piece(gamePanel.loadImage("/sprites/w_queen.png"), "queen", 0, 0, 0));
-		piecesMap.put("P", new Piece(gamePanel.loadImage("/sprites/w_pawn.png"), "pawn", 0, 0, 0));
-		piecesMap.put("R", new Piece(gamePanel.loadImage("/sprites/w_rook.png"), "rook", 0, 0, 0));
-		piecesMap.put("B", new Piece(gamePanel.loadImage("/sprites/w_bishop.png"), "bishop", 0, 0, 0));
-		piecesMap.put("N", new Piece(gamePanel.loadImage("/sprites/w_knight.png"), "knight", 0, 0, 0));
-
-		piecesMap.put("k", new Piece(gamePanel.loadImage("/sprites/b_king.png"), "king", 1, 0, 0));
-		piecesMap.put("q", new Piece(gamePanel.loadImage("/sprites/b_queen.png"), "queen", 1, 0, 0));
-		piecesMap.put("p", new Piece(gamePanel.loadImage("/sprites/b_pawn.png"), "pawn",1, 0, 0));
-		piecesMap.put("r", new Piece(gamePanel.loadImage("/sprites/b_rook.png"), "rook", 1, 0, 0));
-		piecesMap.put("b", new Piece(gamePanel.loadImage("/sprites/b_bishop.png"), "bishop", 1, 0, 0));
-		piecesMap.put("n", new Piece(gamePanel.loadImage("/sprites/b_knight.png"), "knight", 1, 0, 0));
-
 		for (int i=0;i<64;i++) {
-			if (gameHandler.fullBoard.get(i) != "-") {
-				if (piecesMap.containsKey(gameHandler.fullBoard.get(i))) {
-					gameHandler.allPieces.add(piecesMap.get(gameHandler.fullBoard.get(i)));
-				}
+			if (gameHandler.fullBoard.get(i) == "P") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_pawn.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
 			}
+			else if (gameHandler.fullBoard.get(i) == "p") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_pawn.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "R") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_rook.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "r") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_rook.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "B") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_bishop.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "b") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_bishop.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "N") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_knight.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "n") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_knight.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "K") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_king.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "k") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_king.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "Q") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/w_queen.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+			else if (gameHandler.fullBoard.get(i) == "q") {
+				gameHandler.allPieces.add(new Piece(gamePanel.loadImage("/sprites/b_queen.png"), (i % 8) * gamePanel.tileSize, (i / 8) * gamePanel.tileSize));
+			}
+
+			
 		}
 
 
-
-
-
-
-
-
-				
-
-
-		
 
 
 
--- Chess/src/Piece.java
+++ Chess/src/Piece.java
@@ -5,25 +5,15 @@ public class Piece {
     BufferedImage img;
 
 
-    String pieceType;
-
-	int color;
-
-
     int x;
     int y;
 
     
 
 
-    public Piece(BufferedImage img, String pieceType, int color, int x, int y) {
+    public Piece(BufferedImage img, int x, int y) {
 
         this.img = img;
-
-
-        this.pieceType = pieceType;
-		
-		this.color = color;
 
         
         this.x = x;
